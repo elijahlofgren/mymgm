9dfa07d72a88923db5f8b95c7d4249d74dafa4f3
Elijah Lofgren <elijahlofgren@gmail.com>
2016-09-20 20:06:22 -0500

    Start project using generator-aspnetpostgresql
    Created with https://github.com/DeathArrow01/generator-aspnetpostgresql

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using mymgm.Models;
+
+namespace mymgm.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public ApplicationDbContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString =
+                "User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=mymgm;Pooling=true;";
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
