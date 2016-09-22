6b35b55b26d74eb95111500d63af2d1c67c117e6
Elijah Lofgren <elijahlofgren@gmail.com>
2016-09-22 17:11:54 -0500

    update deploy script

--- update_server.sh
+++ update_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+sudo service supervisor stop
 git fetch origin
 git reset --hard origin/master
 dotnet ef database update
 dotnet publish
-sudo service supervisor restart
+sudo service supervisor start
